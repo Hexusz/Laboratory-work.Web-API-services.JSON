c263b55f2b2312262c4becd4c7f1c724c8059b50
Александр <alo0606@mail.ru>
2020-10-20 12:20:21 +0500

    Hello world

---
+++ Laboratory-work.Web-API-services.JSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratory_work.Web_API_services.JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
